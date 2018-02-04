dfe924727bede48acb7e6630f0e59d3cc56a839f
Chris Kaukis <chris@cloudkite.io>
2018-02-04 17:16:00 -0600

    Small clean up

--- test/controllers/tournaments_controller_test.rb
+++ test/controllers/tournaments_controller_test.rb
@@ -1,13 +1,8 @@
 require 'test_helper'
 
 class TournamentsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-
   test "create a tournament" do
     post tournaments_url, params: { number_of_pools: 4, number_of_teams: 14, number_of_rounds: 1 }
-
     assert_response :success
     data = JSON.parse(response.body)
     assert data["teams"].length == 14
